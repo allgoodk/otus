ded8e04d555ba0d56949d51fd919e47192ce44b1
Evgeniy Sulimov <evgeniy.sulimov@renlife.com>
2020-01-20 18:47:19 +0300

    disable em usage  in repo

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -14,6 +14,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use function get_class;
 
 /**
+ * TODO заменить
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
  * @method User|null findOneBy(array $criteria, array $orderBy = null)
  * @method User[]    findAll()
